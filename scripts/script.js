670d42af1bdf7f039c98e59850fc8f644420859a
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2020-11-06 01:58:43 +1000

    top button

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,23 @@
 "use strict"
 
-const playerToggleButton = document.querySelector('.toggle-btn');
+const playerToggleButton = document.querySelector('.button__toggle');
 const playerList = document.querySelector('.playlist');
-const songTextButton = document.querySelector('.song__text');
+const songTextButton = document.querySelector('.button__text');
 
 const headerToggleButton = document.querySelector('.header__button');
 const headerStreaming = document.querySelector('.header__links');
 
 const toggleInfo = () => {
   playerList.classList.toggle('visible');
-  playerToggleButton.classList.toggle('close-btn');
+  playerToggleButton.classList.toggle('button__close');
   songTextButton.classList.toggle('visible-btn');
 }
 
 const toggleStreaming = () => {
-  headerStreaming.classList.toggle('vis-btn');
-  headerToggleButton.classList.toggle('close-btn');
+  headerStreaming.classList.toggle('visible-btn');
+  headerToggleButton.classList.toggle('button__close');
   headerToggleButton.classList.toggle('header__button');
+  headerToggleButton.classList.toggle('button__close_top');
 }
 
 playerToggleButton.addEventListener('click', toggleInfo);
